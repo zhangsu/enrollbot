20a1248c1370535f8f72fd53f8e6a74d5319e154
Su Zhang <me@zhang.su>
2013-06-06 23:21:41 -0700

    Add steps to actually log in

--- bot.rb
+++ bot.rb
@@ -1,4 +1,7 @@
+require 'yaml'
 require 'mechanize'
+
+credential = YAML.load_file('credential.yaml')
 
 login_url = 'https://quest.pecs.uwaterloo.ca/psp/SS/?cmd=login'
 
@@ -6,4 +9,6 @@ agent = Mechanize.new
 agent.ca_file = 'curl-ca-bundle.crt'
 page = agent.get(login_url)
 
-p page.form('login')
+login_form = page.form('login')
+%w(userid pwd).each { |field| login_form[field] = credential[field] }
+p login_form.submit
